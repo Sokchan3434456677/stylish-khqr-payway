975bdc057d44375affde38d64468d004a1069d55
Earsokchan <145833822+Sokchan3434456677@users.noreply.github.com>
2025-03-06 14:55:43 +0700

    Update Fproduct.js

--- src/components/Fproduct.js
+++ src/components/Fproduct.js
@@ -5,7 +5,7 @@ import ModelFade from './ModelFade'; // Import the modal component
 import { useCart } from './CartContext'; // Import the cart context
 import useFetch from './hooks/useFetch'; // Custom hook for fetching products
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/products';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/products';
 
 function Fproduct() {
   const { data: products, error, loading } = useFetch(API_URL); // Using custom hook for fetching data
